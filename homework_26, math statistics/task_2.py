d6451a09ccdc2afe71bb2b1c1d7d325301430bbc
Oleksandra Blyshchak <oleksandra.blyschak@gmail.com>
2023-01-09 12:39:06 +0200

    adding ipynb type
    Signed-off-by: Oleksandra Blyshchak <oleksandra.blyschak@gmail.com>

--- homework_26, math statistics/task_2.py
+++ homework_26, math statistics/task_2.py
@@ -83,9 +83,6 @@ class MathStat():
 
         return empirical_dist_func
 
-
-obj = MathStat([10, 13, 10, 9, 9, 12, 12, 6, 7, 9, 8, 9, 11, 9, 14, 13, 9, 8, 8, 7, 10, 10, 11, 11, 11, 12, 8, 7, 9 ,10, 14, 13, 8, 8, 9, 10, 11, 11, 12, 12])
-
 class Draw:
     def __init__(self, obj: MathStat):
         self.obj = obj
@@ -138,15 +135,6 @@ class Draw:
                 'Wi' : w}
         result = pd.DataFrame.from_dict(data, orient='index', columns=range(1, 10))
         return result
-
-
-
-
-draw_ob = Draw(obj)
-# draw_ob.polygon_of_frequencies()
-# draw_ob.relative_polygon()
-# draw_ob.empirical_dist_func()
-print(draw_ob.table())
 
 
 
