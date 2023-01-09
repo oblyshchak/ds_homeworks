8fe15bac23d8f764b16209a5d55e6ed087438a53
Oleksandra Blyshchak <oleksandra.blyschak@gmail.com>
2023-01-09 12:36:04 +0200

    adding ipynb type
    Signed-off-by: Oleksandra Blyshchak <oleksandra.blyschak@gmail.com>

--- homework_26, math statistics/task_2.py
+++ homework_26, math statistics/task_2.py
@@ -86,8 +86,6 @@ class MathStat():
 
 obj = MathStat([10, 13, 10, 9, 9, 12, 12, 6, 7, 9, 8, 9, 11, 9, 14, 13, 9, 8, 8, 7, 10, 10, 11, 11, 11, 12, 8, 7, 9 ,10, 14, 13, 8, 8, 9, 10, 11, 11, 12, 12])
 
-
-
 class Draw:
     def __init__(self, obj: MathStat):
         self.obj = obj
@@ -126,7 +124,7 @@ class Draw:
         plt.xlim(0)
         plt.xlabel('Xi - axis')
         plt.ylabel('Wi - axis')
-        plt.title('Frequency polygons')
+        plt.title('Relative polygons')
         plt.show()
 
     def table(self):
@@ -145,9 +143,9 @@ class Draw:
 
 
 draw_ob = Draw(obj)
-draw_ob.polygon_of_frequencies()
-draw_ob.relative_polygon()
-draw_ob.empirical_dist_func()
+# draw_ob.polygon_of_frequencies()
+# draw_ob.relative_polygon()
+# draw_ob.empirical_dist_func()
 print(draw_ob.table())
 
 
